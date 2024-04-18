8952edfce41ed88b433c2f60d957ccc99e41aca2
manexdddd <manevergara14@gmail.com>
2024-04-17 19:02:08 -0600

    first

--- routes/web.php
+++ routes/web.php
@@ -5,16 +5,6 @@
 use App\Http\Middleware\ExampleMiddleware;
 use Flipbox\LumenGenerator\Console\MiddlewareMakeCommand;
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It is a breeze. Simply tell Lumen the URIs it should respond to
-| and give it the Closure to call when that URI is requested.
-|
-*/
 
 $router->get('/', function () use ($router) {
     //return $router->app->version();
@@ -30,12 +20,12 @@ function recurso($router,$url,$modelo){
     $router ->get("$url/{id}",$modelo."Controller@show");
     
     $router ->post("$url",$modelo."Controller@store");
-    $router ->put("$url/{id}",$modelo,"Controller@update");
-    $router ->delete("$url/{id}","$modelo,Controller@destroy");
+    $router ->put("$url/{id}",$modelo."Controller@update");
+    $router ->delete("$url/{id}",$modelo."Controller@destroy");
 }
 
 $router->group(['middlewere'=>'auth'],function()use($router){
-recurso($router,'users','Users');
+recurso($router,'users','User');
 recurso($router,'sensors','Sensors');
 recurso($router,'actuators','Actuators');
 });
